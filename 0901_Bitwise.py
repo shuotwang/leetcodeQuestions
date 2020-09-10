5595c8488939a402775142dbdb6b62e2cd333e8a
shuotwang <shuotongwang@gmail.com>
2020-09-10 15:56:18 -0700

    09102020 Update Bitwise questions: LeetCode 338 Counting Bits

--- 0901_Bitwise.py
+++ 0901_Bitwise.py
@@ -28,9 +28,27 @@ def oddOccurance(arr):
     return ans
 
 
+"""
+Update on Sep 10, 2020
+    LeetCode 338. Counting Bits
+    Given a non negative integer number num. 
+    For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation 
+    and return them as an array.
+"""
+
+
+def countBits(num: int) -> [int]:
+    ans = [0]
+
+    for i in range(1, num + 1):
+        ans.append(ans[i // 2] + i % 2)
+    return ans
+
+
 print(getMaxInt())
 print(getMinInt())
 print(mul2(5))
 print(div2(10))
 print(interchange(3, 9))
 print(oddOccurance([1, 2, 3, 2, 3, 1, 3]))
+print(countBits(10))
